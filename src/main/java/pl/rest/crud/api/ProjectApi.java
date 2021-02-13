cec9434c904820c8c5c68319957303fab27a0744
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-02-13 18:22:44 +0100

    Getting project by id is possible.

--- src/main/java/pl/rest/crud/api/ProjectApi.java
+++ src/main/java/pl/rest/crud/api/ProjectApi.java
@@ -1,6 +1,7 @@
 package pl.rest.crud.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ public class ProjectApi {
     @GetMapping
     public List<ProjectDto> getAllSkills() {
         return projectService.getAllProjects();
+    }
+
+    @GetMapping("/{id}")
+    public ProjectDto getProjectById(@PathVariable("id") Long projectId) {
+        return projectService.getProjectById(projectId);
     }
 
     @PostMapping
--- src/main/java/pl/rest/crud/service/ProjectService.java
+++ src/main/java/pl/rest/crud/service/ProjectService.java
@@ -5,6 +5,7 @@ import pl.rest.crud.dto.ProjectDto;
 import pl.rest.crud.mapper.ProjectMapper;
 import pl.rest.crud.repository.ProjectRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,13 @@ public class ProjectService {
                 .stream()
                 .map(projectMapper::toProjectDto)
                 .collect(Collectors.toList());
+    }
+
+    public ProjectDto getProjectById(long id) {
+        return projectRepository
+                .findById(id)
+                .map(projectMapper::toProjectDto)
+                .orElseThrow(() -> new EntityNotFoundException("Project of id: " + id + " not exists!"));
     }
 
     public void addNewProject(ProjectDto project) {
